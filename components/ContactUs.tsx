ce38986260f4bd3c724c546c1788d041b4b277b9
HassanJamalBhatti <hassan29463855@gmail.com>
2026-01-14 19:05:41 +0500

    service

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -30,9 +30,9 @@ export default function ContactUs({ id }: { id?: string })  {
   e.preventDefault();
   setLoading(true);
   setStatusMessage("");
-
+    const API_URL = process.env.NEXT_PUBLIC_API_URL!;
   try {
-    const response = await fetch("http://localhost:5000/api/contact/contact", {
+    const response = await fetch(`${API_URL}/api/contact/contact`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
